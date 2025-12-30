6ca4d4cd817c1e0d657369f814d0f0aee7b71657
Secunda <ego@tlessy.ru>
2025-12-30 19:55:11 +0700

    Testing UDP support
    Signed-off-by: Secunda <ego@tlessy.ru>

--- forwarder.sh
+++ forwarder.sh
@@ -1,4 +1,4 @@
-#!/bin/sh 
+#!/bin/sh
 p1=${START_PORT:-16000}
 p2=${END_PORT:-16799}
 target=${TARGET_IP:-192.168.100.15}
@@ -6,7 +6,7 @@ target=${TARGET_IP:-192.168.100.15}
 echo "Socratting things from $p1 to $p2 to $target"
 
 for p in $(seq $p1 $p2); do
-  socat TCP-LISTEN:$p,fork,reuseaddr TCP:$target:$p &
+  socat TCP-LISTEN:$p,fork,reuseaddr TCP:$target:$p & socat UDP4-LISTEN:$p,reuseaddr,fork UDP4:$target:$p &
 done
 
 wait
